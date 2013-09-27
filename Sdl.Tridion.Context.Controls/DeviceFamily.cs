3348fb0f1aed4ed1b3d7cdc94665ff6beab97ba3
Nuno Linhares <nlinhares@sdl.com>
2013-09-27 13:49:14 +0000

    Added context:Family server control
    the context:Family server control can be used to define server-side
    regions of the page that will only render when the device is in a
    specific family.

---
+++ Sdl.Tridion.Context.Controls/DeviceFamily.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sdl.Tridion.Context.Controls
+{
+    [DefaultProperty("Names")]
+    [ParseChildren(false)]
+    [PersistChildren(true)]
+    [ToolboxData("<{0}:Family Names=\"{1}\" runat=server></{0}:Family>")]
+    public class Family : WebControl
+    {
+        
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("desktop")]
+        [Localizable(true)]
+
+        public string Names
+        {
+            get
+            {
+                String s = (String)ViewState["FamilyNames"];
+                return (s ?? "[" + ID + "]");
+            }
+ 
+            set
+            {
+                ViewState["FamilyNames"] = value;
+                _names = value.Split(',').ToList();
+            }
+        }
+
+        private List<string> _names = new List<string>();
+
+        private ContextEngine _context;
+
+        private bool IsDeviceInFamily
+        {
+            get
+            {
+                if(_context == null)
+                    _context = new ContextEngine();
+                return _names.Contains(_context.DeviceFamily);
+            }
+        }
+
+        protected override void Render(HtmlTextWriter output)
+        {
+            if(IsDeviceInFamily)
+                RenderChildren(output);
+ 
+        }
+    }
+}
